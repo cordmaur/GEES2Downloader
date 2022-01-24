dcb98006c183f77cd1caba3fcec98d0d29133599
Mauricio Cordeiro <cordmaur@gmail.com>
2022-01-24 22:05:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        'setuptools~=58.0.4',
-        'rasterio~=1.2.10',
+        'setuptools>=58',
+        'rasterio>=1.2.10',
         'earthengine-api~=0.1.290',
-        'numpy~=1.21.3',
+        'numpy>=1.20',
         'matplotlib>=3.4.3',
-        'requests~=2.26.0',
-        'geojson~=2.5.0',
-        'tqdm~=4.62.3',
+        'requests>=2.26.0',
+        'geojson>=2.5.0',
+        'tqdm>=4.62.3',
     ]
 )
 
